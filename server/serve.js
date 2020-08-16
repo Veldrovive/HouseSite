d459a2ffeff4b3105e9774900cb8a22e9806ad04
Aidan Dempster <aidan.dempster@gmail.com>
2020-08-15 23:28:16 -0400

    Added a docker file that builds the app and exposes port 3006.

--- server/serve.js
+++ server/serve.js
@@ -9,10 +9,10 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 
-app.use(history())
+app.use(history());
 app.use(express.static(path.join(__dirname, "dist")));
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 let trips;
 try{
